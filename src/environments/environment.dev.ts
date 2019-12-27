2cc94207da83d805f8972680aaeaa4bc23152a3b
Jaydev Thomke <jaydev.thomke@firstcry.com>
2019-12-27 22:33:07 +0530

    first commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://localhost:9000/api', // <Your API base url>
+  imageUrl: 'http://localhost:9000/api/media/image-resize', // <Your API url for image resize>
+  productUrl: 'http://localhost:4205/#/' // <Your store base url>
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://ecobblersapi.westindia.cloudapp.azure.com:9000/api', // <Your API base url>
+  imageUrl: 'http://ecobblersapi.westindia.cloudapp.azure.com:9000/api/media/image-resize', // <Your API url for image resize>
+  productUrl: 'http://ecobblersapi.westindia.cloudapp.azure.com:4200/#/' // <Your store base url>
+
+};
